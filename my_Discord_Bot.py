405edf0d1301eb7af4d9622d46683ffa232304a2
Cavefrank <xueyichao2000@163.com>
2025-02-17 19:46:38 +0800

    1.1

--- my_Discord_Bot.py
+++ my_Discord_Bot.py
@@ -25,11 +25,13 @@ bot = MyBot(
     proxy="http://127.0.0.1:10809"
 )
 
+
 # 反馈登录结果
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
     print("登陆成功")
+
 
 # 自动获取2K图片
 @bot.event
@@ -37,9 +39,26 @@ async def on_message(message):
     if message.author == bot.user:
         return
     await specific_text_check(SAVE_FOLDER, message)
+    # 这里存在一个“拦截”机制，暂时不明白
+    # 不添加下述这行代码的话，command()都会被拦截，且也不是被“if message.author == bot.user”拦截
+    await bot.process_commands(message)
+
+
+# 错误处理机制
+@bot.event
+async def on_command_error(ctx, error):
+    print(f"命令执行错误: {error}")
+    await ctx.send(f"命令执行失败: {str(error)}")
+
+
+# 待完善
+@bot.command()
+async def F(ctx, *, prompt: str): # 需要使用*获取所有输入内容，否则自动按照空格隔开
+    """输入部分提示词，自动发送完整提示词"""
+    await ctx.send(f"/imagine prompt: {prompt} --ar 9:20 --q 2 --v 6.1 --style raw --s 218")
 
 
 if __name__ == "__main__":
     # ✅ 安全启动方式
     # 包含具体Token的代码会被拒绝推送
-    bot.run(input("请输入Token"))
+    bot.run(input("请输入Token:"))
